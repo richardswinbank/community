b1857f76846ccc126430c96c5c643b809261d27c
Richard Swinbank <richard@firefive.io>
2020-10-05 20:45:57 +0100

    Added paging pipeline

--- adf-google-analytics/scripts/dbo.GoogleAnalytics.sql
+++ adf-google-analytics/scripts/dbo.GoogleAnalytics.sql
@@ -19,5 +19,8 @@ CREATE TABLE dbo.GoogleAnalytics (
 , Metric7 DECIMAL(19,5)     NULL
 , Metric8 DECIMAL(19,5)     NULL
 , Metric9 DECIMAL(19,5)     NULL
-, NextPageToken INT NULL
 )
+
+ALTER TABLE dbo.GoogleAnalytics 
+ADD NextPageToken INT NULL
+
