6b664f3726769fe289fdbf132555f21580a5a9c7
Richard Swinbank <richard@firefive.io>
2020-10-25 16:53:53 +0000

    Updated main.tf

--- catch-22-msi-sql/terraform/main.tf
+++ catch-22-msi-sql/terraform/main.tf
@@ -5,13 +5,17 @@ resource "azurerm_resource_group" "rg" {
   tags = var.tags
 }
 
+resource "random_password" "sql_admin_pwd" {
+  length = 50
+}
+
 resource "azurerm_sql_server" "sql" {
   name                         = "${var.environment_name}-sql"
   location                     = azurerm_resource_group.rg.location
   resource_group_name          = azurerm_resource_group.rg.name
   version                      = "12.0"
   administrator_login          = "sqladmin"
-  administrator_login_password = var.sql_admin_pwd
+  administrator_login_password = random_password.sql_admin_pwd.result
   tags = var.tags
 }
 
@@ -30,6 +34,11 @@ resource "azurerm_sql_database" "db" {
   server_name         = azurerm_sql_server.sql.name
   edition             = "Basic"
   tags                = var.tags
+}
+
+output "db_connection_string" {
+  value = "Server=tcp:${azurerm_sql_server.sql.name},1433;Initial Catalog=ExampleDb;User ID=sqladmin;Password=${azurerm_sql_server.sql.administrator_login_password};"
+  sensitive = true  
 }
 
 resource "azurerm_data_factory" "adf" {
--- catch-22-msi-sql/terraform/variables.tf
+++ catch-22-msi-sql/terraform/variables.tf
@@ -1,7 +1,6 @@
 
 # set externally
 variable "environment_name" { type = string }
-variable "sql_admin_pwd" { type = string }
 
 # set in values.auto.tfvars
 variable "resource_location" { type = string }
