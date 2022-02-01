132f30d3da445547478a592e9e32421f5a10f88a
Richard Swinbank <richard@firefive.io>
2022-02-01 06:43:36 +0000

    Added Resolve-Path

--- git-pre-commit-hooks/Sort-TabularModel.ps1
+++ git-pre-commit-hooks/Sort-TabularModel.ps1
@@ -2,6 +2,6 @@ $scriptFolder = $MyInvocation.MyCommand.Path | Split-Path
 Set-Location $scriptFolder
 Import-Module $scriptFolder\HookFunctions\HookFunctions.psm1 -Force
  
-$f = "..\SsasProjects\MyTabularModel\Model.bim"
+$f = Resolve-Path -Path "..\SsasProjects\MyTabularModel\Model.bim"
 ConvertTo-OrderedModel -ModelFile $f
 Invoke-Utility git add $f  # re-stage file after reordering
